20c42e57c65f92f89d972e6ce166be87df0195ff
asharijuang <asharijuang@me.com>
2015-05-20 20:58:47 +0700

    generate comment model polymorphics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520130637) do
+ActiveRecord::Schema.define(version: 20150520134822) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
